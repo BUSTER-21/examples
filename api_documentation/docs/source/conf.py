351f1912f69f63a03a09c0af4978da08c02283a7
Phagun Baya <phagun.baya@gmail.com>
2022-02-15 15:11:45 +0530

    API documentation (#28)
    * adding integration example for ocs and nanop
    
    * adding api doc
    
    * fixing the template
    
    * templating
    
    * templating
    
    * Update index.rst
    
    * api doc
    
    * api docs
    
    * api doc update
    
    * api doc
    
    * addding logo
    
    * fix build failure
    
    * fix failing build
    
    * fixing build
    
    * fixing build
    
    * fixing build
    
    * fixing build
    
    * fixing build
    
    * fixing build
    
    * removing django dep
    
    * fixing build
    
    * fixing build
    
    * fixing indentation
    
    * fixing bold text
    
    * minor change
    
    * adding model api
    
    * output api
    
    * typo
    
    * fixes
    
    * fixes
    
    * typo
    
    * typo fix
    
    * typo
    
    * beautify
    
    * api for episodes
    
    * fixes
    
    * typo fix

---
+++ api_documentation/docs/source/conf.py
@@ -0,0 +1,57 @@
+project = 'Falkonry APIs'
+copyright = '2012-22, Falkonry, Inc'
+author = 'Falkonry'
+
+release = '1.0'
+version = '1.0.0'
+
+master_doc = 'index'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.httpdomain',
+    'sphinx_tabs.tabs',
+    'sphinx-prompt',
+    'notfound.extension',
+    'sphinx_search.extension',
+    'sphinxemoji.sphinxemoji',
+    'sphinx_tabs.tabs',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+html_theme = 'sphinx_rtd_theme'
+html_logo = 'img/favicon.ico'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+epub_show_urls = 'footnote'
+
+html_show_sourcelink = False
+
+language = 'en'
+
+hoverxref_auto_ref = True
+
+notfound_context = {
+    'title': 'Page Not Found',
+    'body': '''
+<h1>Page Not Found</h1>
+<p>Sorry, we couldn't find that page.</p>
+<p>Try using the search box or go to the homepage.</p>
+''',
+}
---
+++ api_documentation/docs/source/index.rst
@@ -0,0 +1,41 @@
+Falkonry APIs
+=============
+
+The Falkonry APIs use :abbr:`REST (Representational State Transfer)`. JSON is returned by all API responses including errors and HTTP response status codes are to designate success and failure. 
+
+Note: You can view the swagger styled API documentation `here <https://app3.falkonry.ai/api/1.2/swagger.app>`__.
+
+Authentication and authorization
+--------------------------------
+
+All the requests to the Falkonry APIs require authentication and are secured by token based authentication.
+
+Token
+~~~~~
+
+The ``Authorization`` HTTP header can be specified with ``Bearer <your-access-token>``
+to authenticate as a user and have the same permissions as the user itself.
+
+.. note::
+
+  You can find your :access token: under API Tokens section.
+
+
+API Examples
+------------
+
+Following are the API examples for a quick integration with Falkonry's output:
+
+.. toctree::
+  :hidden:
+
+   self
+
+.. toctree::
+  :maxdepth: 1
+  :glob:
+
+  examples/raw_points
+  examples/fetch_model
+  examples/fetch_output
+  examples/fetch_episode
