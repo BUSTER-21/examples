fd99e2bff0432151f1b0eab12843e06c5f3b5f88
Lenin <lenin.kumar.s@falkonry.com>
2022-03-30 17:15:33 -0700

    Update compact.py

--- airflow-dags/compact.py
+++ airflow-dags/compact.py
@@ -12,7 +12,7 @@ default_args = {
 
 with DAG('compact-01', default_args=default_args, schedule_interval=None,
          max_active_tasks=30, max_active_runs=30) as dag:
-    load_resources = V1ResourceRequirements(requests={"memory": "18Gi"}, limits={"memory": "18Gi"})
+    load_resources = V1ResourceRequirements(requests={"memory": "35Gi"}, limits={"memory": "35Gi"})
     node_selector = {"loader-node": "true"}
     image_pull_secrets = [k8s.V1LocalObjectReference('falkonry-pull-secret')]
 
