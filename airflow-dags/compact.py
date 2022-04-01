a8697b9231810c0557926ab1630f18c156375798
Lenin <lenin.kumar.s@falkonry.com>
2022-04-01 07:53:06 -0700

    Update compact.py

--- airflow-dags/compact.py
+++ airflow-dags/compact.py
@@ -12,7 +12,7 @@ default_args = {
 
 with DAG('compact-01', default_args=default_args, schedule_interval=None,
          max_active_tasks=30, max_active_runs=30) as dag:
-    load_resources = V1ResourceRequirements(requests={"memory": "35Gi"}, limits={"memory": "35Gi"})
+    load_resources = V1ResourceRequirements(requests={"memory": "18Gi"}, limits={"memory": "18Gi"})
     node_selector = {"loader-node": "true"}
     image_pull_secrets = [k8s.V1LocalObjectReference('falkonry-pull-secret')]
 
@@ -53,9 +53,9 @@ with DAG('compact-01', default_args=default_args, schedule_interval=None,
         envs.append(k8s.V1EnvVar(name=k, value=v))
 
     envs.append(k8s.V1EnvVar(name="falkonry_clue_livestream_non_cloud", value="true"))
-    envs.append(k8s.V1EnvVar(name="falkonry_tiling_bulk_compact_concurrency", value="10"))
+    envs.append(k8s.V1EnvVar(name="falkonry_tiling_bulk_compact_concurrency", value="30"))
     envs.append(k8s.V1EnvVar(name="AWS_DYN_ENDPOINT", value="http://localhost:8000"))
-    compact_resources = V1ResourceRequirements(requests={"memory": "35Gi"}, limits={"memory": "35Gi"})
+    compact_resources = V1ResourceRequirements(requests={"memory": "18Gi"}, limits={"memory": "18Gi"})
 
     compact = KubernetesPodOperator(
         namespace='falkonry',
